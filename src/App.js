abdabf5e32c30b1dbcd24cac0a2dfe23d7413f08
MADHAV JHAMB <mjhamb_be20@thapar.edu>
2023-12-16 01:45:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Home from "./Home";
+import Singlemovie from "./Singlemovie";
+import Error from "./Error";
+import {Routes,Route} from "react-router-dom";
+import "./App.css"
+const App = () => {
+  return (
+    <>
+       
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="movie/:id" element={<Singlemovie/>} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        
+    </>
+  )
+}
+
+export default App
+
+
+
